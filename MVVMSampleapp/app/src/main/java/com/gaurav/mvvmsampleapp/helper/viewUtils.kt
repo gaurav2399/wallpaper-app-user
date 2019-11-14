a2a693782466e038e4cfc4dc20469955de784d0e
Gaurav <gs926189@gmail.com>
2019-11-14 19:32:38 +0530

    initial commit

--- MVVMSampleapp/app/src/main/java/com/gaurav/mvvmsampleapp/helper/viewUtils.kt
+++
@@ -1,8 +0,0 @@
-package com.gaurav.mvvmsampleapp.helper
-
-import android.content.Context
-import android.widget.Toast
-
-fun Context.toast(message: String, len: Int = Toast.LENGTH_SHORT){
-    Toast.makeText(this,message,len).show()
-}
--- MVVMSampleapp/app/src/main/java/com/gaurav/mvvmsampleapp/ui/auth/AuthListener.kt
+++
@@ -1,7 +0,0 @@
-package com.gaurav.mvvmsampleapp.ui.auth
-
-interface AuthListener {
-    fun onStarted()
-    fun onSuccess()
-    fun onFailure(message: String)
-}
